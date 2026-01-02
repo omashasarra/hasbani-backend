9a96c2f9328b352df779f1b76ad7bfc308c83561
omashasarra <omashasara@gmail.com>
2026-01-03 01:45:51 +0200

    Downgrade to Express 4 to fix wildcard route issue

--- server.js
+++ server.js
@@ -71,8 +71,16 @@ app.use(
   })
 );
 
-// Handle preflight requests
-app.options("*", cors());
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
 
 // JSON body parser
 app.use(express.json({ limit: "10mb" }));
